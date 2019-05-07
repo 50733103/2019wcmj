be52f902bfca2ddcb62a9c4d79f9b9073999af42
Feng0605 <50733103@gm.nfu.edu.tw>
2019-05-07 11:42:41 +0800

    add final project info

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'head 1', 'text': '我的第一個javascript程式 \n \n \n 按一下 \n \n', 'tags': '', 'url': 'head 1.html'}]};
+var tipuesearch = {"pages": [{'title': 'head 1', 'text': '我的第一個javascript程式 \n \n \n 按一下 \n \n \n \n <p><button onclick="myFunction()">Try it</button></p>\n<p id="demo"></p>\n<script>// <![CDATA[\nfunction myFunction() {\n\xa0\xa0document.getElementById("demo").innerHTML = Math.floor(Math.random()*10);\n}\n// ]]></script> \n', 'tags': '', 'url': 'head 1.html'}]};
